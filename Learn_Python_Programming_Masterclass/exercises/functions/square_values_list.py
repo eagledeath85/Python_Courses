465bd5b631444a3a18d1fdf25740dbe1e1cb1e4f
adrienallouche <adrien.allouche@gmail.com>
2022-12-06 15:51:33 +0200

    square_values_list.py: function that returns a list where the elements are the square values of the elements of the original list. Generates a list of random size populated with random values

--- Learn_Python_Programming_Masterclass/exercises/functions/square_values_list.py
+++ Learn_Python_Programming_Masterclass/exercises/functions/square_values_list.py
@@ -1,5 +1,12 @@
-input_list = [1, 2, 3, 4, 5]
+from random import random, randint
 
+# Defining input_list size
+input_list = []
+for i in range(randint(0, 10)):
+    # Populating input_list with random values from 1 to 30
+    n = randint(1, 30)
+    input_list.append(n)
+print(input_list)
 
 def square_values(list_square_values_from: list) -> list:
     new_list = []
